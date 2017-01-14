9efddf3be63a9e07ca0827bae34099e05c04c884
andy1729 <ujjawalanand1729@gmail.com>
2017-01-14 22:45:05 +0530

    added list adapter

--- app/src/main/java/com/example/ujjawal/friendlychat/model/Message.java
+++ app/src/main/java/com/example/ujjawal/friendlychat/model/Message.java
@@ -5,4 +5,37 @@ package com.example.ujjawal.friendlychat.model;
  */
 
 public class Message {
+    private String uid;
+    private String text;
+    private String photoUrl;
+    private String author;
+
+    public Message() {}
+
+    public Message(String uid, String text, String author, String photoUrl) {
+        this.uid = uid;
+        this.text = text;
+        this.author = author;
+        this.photoUrl = photoUrl;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
 }
--- app/src/main/java/com/example/ujjawal/friendlychat/model/User.java
+++ app/src/main/java/com/example/ujjawal/friendlychat/model/User.java
@@ -5,4 +5,26 @@ package com.example.ujjawal.friendlychat.model;
  */
 
 public class User {
+    private String name;
+    private String profilePicUrl;
+    private String email;
+
+    public User() {}
+    public User(String name, String email, String profilePicUrl) {
+        this.name = name;
+        this.email = email;
+        this.profilePicUrl = profilePicUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getProfilePicUrl() {
+        return profilePicUrl;
+    }
 }
